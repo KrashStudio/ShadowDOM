afbeb6b92e441f0063850787afc9cb3192182c01
TinOo512 <etienne@kontestapp.com>
2015-10-09 11:17:39 +0200

    fix(wrappers): fix mixin the `DocumentFragment` because of `non configurable` property

--- src/wrappers/generic.js
+++ src/wrappers/generic.js
@@ -12,6 +12,32 @@
   var registerObject = scope.registerObject;
 
   var DocumentFragment = registerObject(document.createDocumentFragment());
+  var prototype = Object.create(Object.getPrototypeOf(DocumentFragment.prototype), {
+    constructor: {
+      value: DocumentFragment,
+      writable: true,
+      enumerable: false,
+      configurable: true
+    }
+  });
+
+  var names = Object.getOwnPropertyNames(DocumentFragment.prototype);
+
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var descriptor = Object.getOwnPropertyDescriptor(DocumentFragment.prototype, name);
+
+    if (descriptor.configurable || (
+        !ParentNodeInterface.hasOwnProperty(name) &&
+        !SelectorsInterface.hasOwnProperty(name) &&
+        !GetElementsByInterface.hasOwnProperty(name)
+      )) {
+      Object.defineProperty(prototype, name, descriptor);
+    }
+  }
+
+  DocumentFragment.prototype = prototype;
+
   mixin(DocumentFragment.prototype, ParentNodeInterface);
   mixin(DocumentFragment.prototype, SelectorsInterface);
   mixin(DocumentFragment.prototype, GetElementsByInterface);
